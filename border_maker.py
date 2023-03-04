2011ef576a616e93e8e35b7d961421b8d7af8fb0
AlinaRechina <shambratona@gmail.com>
2023-03-04 12:13:41 +0300

    better borders

--- border_maker.py
+++ border_maker.py
@@ -5,11 +5,19 @@
 #!pip install konlpy
 from konlpy.tag import Twitter
 from konlpy.tag import Kkma
+import csv
 
 twitter = Twitter()
 kkma = Kkma()
 
-def separator(text):
+def intruser(word, final_trans):
+    # ЗДЕСЬ НУЖНО БУДЕТ ПЕРЕФОРМИРОВЫВАТЬ СИНОКОРЕЙСКИЕ СЛОВА (ЗВОНКИЕ -> TENSE)
+    ready_word = ''
+    for char in word:
+        ready_word += '-' + final_trans[char]
+    return ready_word.strip('-')
+
+def separator(text, ft):
     syll_dict = dict()
 
     with open('final_trans.csv') as csvfile:
@@ -29,20 +37,21 @@ def separator(text):
     for entity in twit_morph:
         print(entity)
         if entity[1] in lil_morphs:
-            good_text = good_text.strip(" /-#") + '-' + entity[0] + '#'
+            good_text = good_text.strip(" /-#") + '-' + intruser(entity[0], 
+                                                                 ft) + '#'
 
         elif entity[1] in end_morphs:
             if good_text.endswith('/ '):
-                good_text += entity[0] + ' / '
+                good_text += intruser(entity[0], ft) + ' / '
             else:
-                good_text += entity[0] + ' / '
+                good_text += intruser(entity[0], ft) + ' / '
 
         elif entity[1] == 'Adjective' or entity[1] == 'Verb':
             # проверяем, аттрибутивное или предикативное употребление
-            if kkma.pos(entity[0])[-1][1]:
-                good_text += entity[0] + '#'
+            if 'ETD' in kkma.pos(entity[0])[-1][1]:
+                good_text += intruser(entity[0], ft) + '#'
             else:
-                good_text += entity[0] + ' / '
+                good_text += intruser(entity[0], ft) + ' / '
 
         elif entity[1] == 'Punctuation':
             good_text = good_text.strip(" /-#") + ' / '
@@ -50,7 +59,17 @@ def separator(text):
             pass
 
         else:
-            good_text += entity[0] + '#'
+            good_text += intruser(entity[0], ft) + '#'
 
 
     return good_text.strip(' /#') + ' / '
+
+final_trans = dict()
+
+with open('final_trans.csv', 'r', encoding='utf-8') as ft_file:
+    spamreader = csv.reader(ft_file)
+    for row in spamreader:
+        final_trans[row[0]] = row[2]
+
+#text = '예쁜 언니가 약하고 넉넉해요'
+#print(separator(text, final_trans))
