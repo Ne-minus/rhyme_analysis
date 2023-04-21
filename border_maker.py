08681d4a4a1285b07fc7844cfb8e812c41fc87e4
Ne-minus <neminova2.0@gmail.com>
2023-04-21 19:51:07 +0300

    made classes

--- border_maker.py
+++ border_maker.py
@@ -6,93 +6,94 @@ from konlpy.tag import Kkma
 import csv
 import re
 
-twitter = Twitter()
-kkma = Kkma()
+class BorderMaker():
 
-def intruser(word, final_trans):
-    ready_word = ''
-    for char in word:
-        ready_word += '-' + final_trans[char]
-    return ready_word.strip('-')
+    def __init__(self):
+        self.twitter = Twitter()
+        self.kkma = Kkma()
 
-def separator(text, ft):
-    syll_dict = dict()
+        self.final_trans = dict()
+        with open('final_trans.csv', 'r', encoding='utf-8') as ft_file:
+          spamreader = csv.reader(ft_file)
+          for row in spamreader:
+              self.final_trans[row[0]] = row[2]
 
-    with open('final_trans.csv') as csvfile:
-        spamreader = csv.reader(csvfile)
-        sylls = list(spamreader)
-        for s in sylls:
-            syll_dict[s[0]] = s[2]
 
-    good_text= ' '
-    twit_morph = twitter.pos(text, norm = True)
+    def intruser(self, word: str) -> str:
+        ready_word = ''
+        for char in word:
+            ready_word += '-' + self.final_trans[char]
+        return ready_word.strip('-')
 
-    lil_morphs = ('Josa', 'Suffix', 'Eomi', 'PreEomi')
-    end_morphs = ('Exclamation', 'Conjunction', 'Eomi', 'PreEomi')
-    bad = ('Foreign', 'Alpha', 'Number', 'Unknown', 'KoreanParticle', 
-           'Hashtag', 'ScreenName', 'Email', 'URL')
+    def separator(self, text: str) -> str:
+        syll_dict = dict()
 
-    for entity in twit_morph:
-        print(entity)
-        if entity[1] in lil_morphs:
-            if entity[0] == '의':
-                good_text = good_text.strip(" /-#") + '-ɛ#' # генитив
-            else:
-                good_text = good_text.strip(" /-#") + '-' + intruser(entity[0], 
-                                                                 ft) + '#'
+        with open('final_trans.csv') as csvfile:
+            spamreader = csv.reader(csvfile)
+            sylls = list(spamreader)
+            for s in sylls:
+                syll_dict[s[0]] = s[2]
 
-        elif entity[1] in end_morphs:
-            if good_text.endswith('/ '):
-                good_text += intruser(entity[0], ft) + ' / '
-            else:
-                good_text += intruser(entity[0], ft) + ' / '
+        good_text = ' '
+        twit_morph = self.twitter.pos(text, norm=True)
 
-        elif entity[1] == 'Adjective' or entity[1] == 'Verb':
-            # отглагольные существительные должны вести себя как
-            # существительные
-            if entity[0].endswith('ki'):
-                good_text += intruser(entity[0], ft) + '#'
-            else:
-                tr = intruser(entity[0], ft)
-                if 'ɾ-ke-jo' in tr:
-                    # ㄹ게요
-                    rtr = ''.join(reversed(list(tr)))
-                    rtr = rtr.replace('ek-ɾ', 'ek͈-ɾ', 1)
-                    tr = ''.join(reversed(list(rtr)))
+        lil_morphs = ('Josa', 'Suffix', 'Eomi', 'PreEomi')
+        end_morphs = ('Exclamation', 'Conjunction', 'Eomi', 'PreEomi')
+        bad = ('Foreign', 'Alpha', 'Number', 'Unknown', 'KoreanParticle',
+               'Hashtag', 'ScreenName', 'Email', 'URL')
 
-                elif 'ɾ-kʌ-jɐ' in tr or 'ɾ-kʌ-je-jo' in tr:
-                    # ㄹ거(예요 / 야)
-                    rtr = ''.join(reversed(list(tr)))
-                    rtr = rtr.replace('ʌk-ɾ', 'ʌk͈-ɾ', 1)
-                    tr = ''.join(reversed(list(rtr)))
-
-                elif 'm-tɐ-ko' in tr or 'm-tɐ /' in tr:
-                    # ㅁ다
-                    rtr = ''.join(reversed(list(tr)))
-                    rtr = rtr.replace('ɐt-m', 'ɐt͈-m', 1)
-                    tr = ''.join(reversed(list(rtr)))
-
-                # проверяем, аттрибутивное или предикативное употребление
-                if 'ETD' in kkma.pos(entity[0])[-1][1]:
-                    good_text += tr + '#'
+        for entity in twit_morph:
+            print(entity)
+            if entity[1] in lil_morphs:
+                if entity[0] == '의':
+                    good_text = good_text.strip(" /-#") + '-ɛ#'  # генитив
                 else:
-                    good_text += tr + ' / '
-            
-              
-        elif entity[1] == 'Punctuation':
-            good_text = good_text.strip(" /-#") + ' / '
-        elif entity[1] in bad:
-            pass
+                    good_text = good_text.strip(" /-#") + '-' + self.intruser(entity[0]) + '#'
 
-        else:
-            good_text += intruser(entity[0], ft) + '#'
+            elif entity[1] in end_morphs:
+                if good_text.endswith('/ '):
+                    good_text += self.intruser(entity[0]) + ' / '
+                else:
+                    good_text += self.intruser(entity[0]) + ' / '
+
+            elif entity[1] == 'Adjective' or entity[1] == 'Verb':
+                # отглагольные существительные должны вести себя как
+                # существительные
+                if entity[0].endswith('ki'):
+                    good_text += self.intruser(entity[0]) + '#'
+                else:
+                    tr = self.intruser(entity[0])
+                    if 'ɾ-ke-jo' in tr:
+                        # ㄹ게요
+                        rtr = ''.join(reversed(list(tr)))
+                        rtr = rtr.replace('ek-ɾ', 'ek͈-ɾ', 1)
+                        tr = ''.join(reversed(list(rtr)))
+
+                    elif 'ɾ-kʌ-jɐ' in tr or 'ɾ-kʌ-je-jo' in tr:
+                        # ㄹ거(예요 / 야)
+                        rtr = ''.join(reversed(list(tr)))
+                        rtr = rtr.replace('ʌk-ɾ', 'ʌk͈-ɾ', 1)
+                        tr = ''.join(reversed(list(rtr)))
+
+                    elif 'm-tɐ-ko' in tr or 'm-tɐ /' in tr:
+                        # ㅁ다
+                        rtr = ''.join(reversed(list(tr)))
+                        rtr = rtr.replace('ɐt-m', 'ɐt͈-m', 1)
+                        tr = ''.join(reversed(list(rtr)))
+
+                    # проверяем, аттрибутивное или предикативное употребление
+                    if 'ETD' in self.kkma.pos(entity[0])[-1][1]:
+                        good_text += tr + '#'
+                    else:
+                        good_text += tr + ' / '
 
 
-    return good_text.strip(' /#') + ' / '
+            elif entity[1] == 'Punctuation':
+                good_text = good_text.strip(" /-#") + ' / '
+            elif entity[1] in bad:
+                pass
 
-final_trans = dict()
+            else:
+                good_text += self.intruser(entity[0]) + '#'
 
-with open('final_trans.csv', 'r', encoding='utf-8') as ft_file:
-    spamreader = csv.reader(ft_file)
-    for row in spamreader:
-        final_trans[row[0]] = row[2]
+        return good_text.strip(' /#') + ' / '
